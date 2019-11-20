132faac68296b740e3d1134b156607a770cac03b
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-11-20 18:24:01 +0100

    done without css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'chez chiara',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'italian',
+  },
+  {
+    name:         'chez alex',
+    address:      '1 rue sympa',
+    category:     'belgian',
+  },
+  {
+    name:         'chez marco',
+    address:      '1 rue cool',
+    category:     'chinese',
+  },
+  {
+    name:         'chez ben',
+    address:      '1 rue pas',
+    category:     'japanese',
+  },
+  {
+    name:         'chez tamaire',
+    address:      '2 bd tonpaire',
+    category:     'french',
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
